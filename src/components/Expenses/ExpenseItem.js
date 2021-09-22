3c69d79e3286a792fd72668f086175197332aab0
Maxwell Miller <maxwell@maxwellm@adobe.com>
2021-09-22 15:03:45 -0600

    Added first button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,9 +1,12 @@
+import React from 'react';
 
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./css/ExpenseItem.css";
 
-
+const handleClick = () => {
+  console.log("clicked");
+};
 
 const ExpenseItem = (props) => {
   return (
@@ -13,6 +16,7 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={handleClick}>Change Title</button>
     </Card>
   );
 };
