127be5218ab096a35a4b2d9844f4dab25a7b1d63
Maxwell <maxwell@layer8games.com>
2021-09-22 19:42:14 -0700

    Added button click count

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./css/ExpenseItem.css";
 
-const handleClick = () => {
-  console.log("clicked");
-};
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    let newCount = count + 1;
+    setCount(newCount);
+    console.log(title);
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
+        <h1>{count}</h1>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={handleClick}>Change Title</button>
